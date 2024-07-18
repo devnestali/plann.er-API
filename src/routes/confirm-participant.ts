32f0c4e93004f7f00831f3b2c6e2f922ae3e826e
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-18 18:50:57 +0200

    Updating file .env and creating new file env.ts for do a schema for all URL in routes

--- src/routes/confirm-participant.ts
+++ src/routes/confirm-participant.ts
@@ -3,6 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmParticipants(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/participants/:participantId/confirm', {
@@ -25,7 +26,7 @@ export async function confirmParticipants(app: FastifyInstance) {
     }
 
     if(participant.is_confirmed) {
-      return reply.redirect(`http://localhost:3000/trips/${participant.tripId}`)
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.tripId}`)
     }
 
     await prisma.participant.update ({
@@ -33,6 +34,6 @@ export async function confirmParticipants(app: FastifyInstance) {
       data: { is_confirmed: true},
     })
 
-    return reply.redirect(`http://localhost:3000/trips/${participant.tripId}`)
+    return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.tripId}`)
   })
 }
